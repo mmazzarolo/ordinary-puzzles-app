c1c186541176858c0978984b2ca8b6b3aad4ad94
mmazzarolo <me@mazzarolomatteo.com>
2021-08-01 19:42:10 +0200

    Initial electron setup

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,10 @@
 const webpack = require("webpack");
 const { override, addWebpackPlugin } = require("customize-cra");
 
+// Rename the CRA entry point to avoid confustion between it and the native one.
+const paths = require("react-scripts/config/paths");
+paths.appIndexJs = `${paths.appSrc}/index.web.ts`;
+
 module.exports = override(
   addWebpackPlugin(
     new webpack.DefinePlugin({
@@ -8,6 +12,7 @@ module.exports = override(
         process.env.NODE_ENV || "development"
       ),
       __DEV__: process.env.NODE_ENV !== "production",
+      __ELECTRON__: !!process.env.ELECTRON,
     })
   )
 );
