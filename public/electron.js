a924cf21bbf9180a9c5af29a80a177bfd196825e
mmazzarolo <me@mazzarolomatteo.com>
2021-08-11 17:53:57 +0200

    Updated electron packaging setup

--- public/electron.js
+++ public/electron.js
@@ -1,33 +1,33 @@
 const { app, BrowserWindow, protocol } = require("electron");
-
 const path = require("path");
 const url = require("url");
 
 let mainWindow;
 
+const isDev = !!process.env.ELECTRON_START_URL;
+
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1024,
     height: 768,
-    webPreferences: {
-      nodeIntegration: true,
-    },
   });
 
   mainWindow.loadURL(
     process.env.ELECTRON_START_URL ||
       url.format({
-        pathname: path.join(__dirname, "/../build/index.html"),
+        pathname: path.join(__dirname, "../build/index.html"),
         protocol: "file:",
         slashes: true,
       })
   );
 
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
   mainWindow.on("closed", () => {
     mainWindow = null;
   });
-
-  mainWindow.webContents.openDevTools();
 }
 
 // Call when we're ready to create browser windows.
